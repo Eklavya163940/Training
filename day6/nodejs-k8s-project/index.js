c4986f9730d08bdad558379d048bb097233fb658
Eklavya163940 <eklavya.agal@einfochips.com>
2024-07-16 14:38:34 +0530

    Update main route message

--- day6/nodejs-k8s-project/index.js
+++ day6/nodejs-k8s-project/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.get('/', (req, res) => {
+    res.send('Hello, Kubernetes! Updated version.');
+});
+
 app.get('/newroute', (req, res) => {
     res.send('This is a new route!');
 });
